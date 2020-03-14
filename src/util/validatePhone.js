c1071ff5db35038c2205be59d348ed81ef7ad840
Jacob Robinson <95jacob07@gmail.com>
2020-03-14 16:47:23 -0700

    better variable naming

--- src/util/validatePhone.js
+++ src/util/validatePhone.js
@@ -1,2 +1,2 @@
-export default string =>
-  /^(1?\s?(\d{3}|\(\d{3}\)))[-?|\s?]?\d{3}[-?|\s?]?\d{4}$/.test(string);
+export default num =>
+  /^(1?\s?(\d{3}|\(\d{3}\)))[-?|\s?]?\d{3}[-?|\s?]?\d{4}$/.test(num);
